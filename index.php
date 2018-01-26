d67a3914607195e3d66e50f3dcd76a1b54d70f6d
Joël ZHU <Jol@DESKTOP-5E5HDMK>
2018-01-26 18:07:10 +0100

    Ajout de fichiers de gestion.

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
-echo "Hello World";
+
+require_once 'autoload.inc.php';
+
+// Création d'une page web avec le titre
+$webpage = new WebPage("Welcome to Joel ZHU's personal website");
+
+// Ajout du contenu
+$webpage->appendContent("Hello world");
+
+// Affichage de la page
+echo $webpage->toHTML();
---
+++ myPDO.include.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once 'autoload.inc.php' ;
+
+myPDO::setConfiguration('mysql:host=mysql;dbname=zhu0001;charset=utf8', 'zhu0001', 'azerty03') ;
